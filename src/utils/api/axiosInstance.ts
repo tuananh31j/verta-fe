28f6aa3ccaeca8ae371c6a14453c31a6d40bb7a8
nguyennhuyenn <nguyennhuyen384@gmail.com>
2025-02-17 04:59:53 +0700

    feat: thêm access token vào header request

--- src/utils/api/axiosInstance.ts
+++ src/utils/api/axiosInstance.ts
@@ -3,12 +3,25 @@ import axios, { AxiosResponse } from 'axios';
 import { Params } from 'react-router-dom';
 import queryString from 'query-string';
 import { envProcess } from '~/constants/env';
+import { getAccessToken } from './apiHelpers';
 const axiosOptions = {
     baseURL: envProcess.api,
     withCredentials: true,
     paramsSerializer: (params: Params) => queryString.stringify(params),
 };
 const instance = axios.create(axiosOptions);
+
+instance.interceptors.request.use(
+    (config) => {
+        const accessToken = getAccessToken();
+        if (config && config.headers && accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
 
 instance.interceptors.response.use(
     <T>(response: AxiosResponse<T>): T => response.data,
