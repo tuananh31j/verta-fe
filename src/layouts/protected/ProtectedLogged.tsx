48de03838ae1085f77528062b8c236df4cd657e4
tran-tuan-vu <chipucutvu123@gmail.com>
2025-02-17 14:08:27 +0700

    feat: xác thực tài khoản và bảo vệ route khi đã đăng nhập

---
+++ src/layouts/protected/ProtectedLogged.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useTypedSelector } from '~/store/store';
+
+export default function ProtectedLogged({ children }: { children: React.ReactNode }) {
+    const user = useTypedSelector((state) => state.auth.user);
+    if (user) {
+        return <Navigate to={'/'} />;
+    }
+    return children;
+}
