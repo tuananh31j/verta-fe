3794fac93d4f20a130e7127cef3b95adbc0d54eb
tran-tuan-vu <chipucutvu123@gmail.com>
2025-02-19 02:19:42 +0700

    fix: thông báo lỗi khi sản phẩm hết hàng mà vẫn cố thêm vào giỏ hàng

--- src/pages/ProductDetail/_components/ActionProductDetail.tsx
+++ src/pages/ProductDetail/_components/ActionProductDetail.tsx
@@ -54,6 +54,7 @@ export default function ActionProductDetail({
         if (!firstAvailableColor) {
             setOutOfStock(true);
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [setColorVariant, variants]);
 
     useEffect(() => {
