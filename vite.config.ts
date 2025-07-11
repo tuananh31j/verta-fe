d17813dd85caa69e85acd9c5aaacb639203ff352
tuananh31j <tuananh31j@gmail.com>
2025-07-11 15:01:00 +0700

    fix: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
+
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, './src'),
+    plugins: [tailwindcss(), react()],
+    server: {
+        port: 3000,
     },
-  },
+    resolve: {
+        alias: {
+            '~': path.resolve(__dirname, './src'),
+        },
+    },
 });
